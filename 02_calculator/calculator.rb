2c722a45805b7c8fc60acf9e1c197fa059af1943
lchen90 <43580876+lchen90@users.noreply.github.com>
2019-03-03 14:29:31 -0600

    Add files via upload

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
 #write your code here
+def add(a,b)
+  a + b
+end
+
+def subtract(a,b)
+  a - b
+end
+
+def sum(array)
+  array.inject(0) { |total, array| total + array}
+end
+
+def multiply(a, b, *c)
+  product = a * b
+  c.reduce(product) {|num, product| num * product}
+end
+
+def power(a,b)
+  a**b
+end
+
+def factorial(num)
+  if num <= 1
+    1
+  else
+    num * factorial(num-1)
+end
+end
